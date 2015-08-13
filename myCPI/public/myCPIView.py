69632f10849724ec2595438a680acce6a917ee05
emperorfrost <dilip.hari@gmail.com>
2015-08-13 19:29:11 -0400

    myCPIView.py 1. removed unused includes 2. moved computation of budget sum into a helper function 3. added function to plot user entered budget shares in a pie chart
    style.css
    1. entries for chart
    
    graph_highcharts.js
    1. new file to take document variables and pass it onto highcharts and produce charts!
    2. Since charts are plotted after POST, the function is inside a $(document).ready block
    
    mycpi.html
    1. cleaned up the template
    2. using {% block js %} tags to include highcharts
    3. added an if block that loads only on POST. This block takes the document variables injected when this template is rendered, assigns them to javascript variables and in turn passes them on to graph_highcharts.js
    4. reverted the extends block to extend from layout.html rather than layout_mvp.html

--- myCPI/public/myCPIView.py
+++ myCPI/public/myCPIView.py
@@ -4,8 +4,7 @@ from flask import (Blueprint, request, render_template, flash, url_for,
                     redirect, session)
 from myCPI.public.myCPIForms import BudgetShareForm
 
-from myCPI.utils import flash_errors
-from flask import flash
+import json
 
 blueprint = Blueprint('mycpi', __name__, static_folder="../static")
 
@@ -13,19 +12,21 @@ blueprint = Blueprint('mycpi', __name__, static_folder="../static")
 @blueprint.route("/mycpi", methods=["GET", "POST"])
 def enterBudgetShare():
     form = BudgetShareForm(request.form)
-    sum_error = None
+    share_data = {'None': None}
     if request.method == "POST":
         if form.validate_on_submit():
             my_cpi = compute_cpi(form)
-            sum_error = None
+            share_data = plot_shares(form)
         else:
-            if form.sum_error:
-                sum_error = "Budget shares don't sum to 1.0. Please enter inputs for each category as a share of your total budget'"
             my_cpi = None
     elif request.method == "GET":
         my_cpi = None
-        sum_error = None  
-    return render_template("public/mycpi.html", form=form, my_cpi=my_cpi, sum_error=sum_error)
+    return render_template("public/mycpi.html", form=form, my_cpi=my_cpi, share_data=share_data)
+
+def get_budget_sum(form):
+    budget_sum = (form.food_share.data+form.housing_share.data+form.apparel_share.data+form.edu_share.data+ \
+        form.transportation_share.data+form.medical_share.data+form.recreation_share.data+form.other_share.data)    
+    return budget_sum
     
 def compute_cpi(form):
     component_indexes = {'food':246.245,\
@@ -36,21 +37,51 @@ def compute_cpi(form):
         'medical_care': 446.271,\
         'recreation': 116.395,\
         'other': 415.022}
-    budget_sum = (form.food_share.data+form.housing_share.data+form.apparel_share.data+form.edu_share.data+ \
-        form.transportation_share.data+form.medical_share.data+form.recreation_share.data+form.other_share.data)
+    
+    ref_indexes = {'food':100,\
+        'housing':100,\
+        'apparel': 100,\
+        'edu': 100,\
+        'transport': 100,\
+        'medical_care': 100,\
+        'recreation': 100,\
+        'other': 100}
+    
+    budget_sum = get_budget_sum(form)
 
-    wgted_sum = (form.food_share.data/budget_sum * component_indexes['food']/100 + \
-        form.housing_share.data/budget_sum * component_indexes['housing']/100 + \
-        form.apparel_share.data/budget_sum * component_indexes['apparel']/100 + \
-        form.edu_share.data/budget_sum * component_indexes['edu']/100 + \
-        form.transportation_share.data/budget_sum * component_indexes['transport']/100 + \
-        form.medical_share.data/budget_sum * component_indexes['medical_care']/100 + \
-        form.recreation_share.data/budget_sum * component_indexes['recreation']/100 + \
-        form.other_share.data/budget_sum * component_indexes['other']/100)
+    wgted_sum = (form.food_share.data/budget_sum) * (component_indexes['food']/ref_indexes['food']) + \
+        (form.housing_share.data/budget_sum) * (component_indexes['housing']/ref_indexes['housing']) + \
+        (form.apparel_share.data/budget_sum) * (component_indexes['apparel']/ref_indexes['apparel']) + \
+        (form.edu_share.data/budget_sum) * (component_indexes['edu']/ref_indexes['edu']) + \
+        (form.transportation_share.data/budget_sum) * (component_indexes['transport']/ref_indexes['transport']) + \
+        (form.medical_share.data/budget_sum) * (component_indexes['medical_care']/ref_indexes['medical_care']) + \
+        (form.recreation_share.data/budget_sum) * (component_indexes['recreation']/ref_indexes['recreation']) + \
+        (form.other_share.data/budget_sum) * (component_indexes['other']/ref_indexes['other'])
     
     inflation = wgted_sum * 100
     return round(inflation,3)
 
+def plot_shares(form, chartID='chartID', chart_type='pie', chart_height=500):
+    budget_shares = {}
+    budget_shares['title'] = {"text": "Visualization of budget shares"}
+    budget_sum = float(get_budget_sum(form))
+    budget_shares['series'] = json.dumps([{'name': "Budget Share",\
+            'colorByPoint': True,\
+            'data':[\
+                {"name":"Food/Beverage", "y": round(100* form.food_share.data/budget_sum, 2)},\
+                {"name":"Housing", "y": round(100*form.housing_share.data/budget_sum, 2)},\
+                {"name":"Apparel", "y": round(100*form.apparel_share.data/budget_sum, 2)},\
+                {"name":"Transportation", "y": round(100*form.transportation_share.data/budget_sum, 2)},\
+                {"name":"Medical Care", "y": round(100*form.medical_share.data/budget_sum, 2)},\
+                {"name":"Recreation", "y": round(100*form.recreation_share.data/budget_sum, 2)},\
+                {"name":"Education", "y": round(100*form.edu_share.data/budget_sum, 2)},\
+                {"name":"Other Services", "y": round(100* form.other_share.data/budget_sum, 2)}]}])
+    budget_shares['page_type'] = "graph"
+    budget_shares['chart'] = {"renderTo": chartID, "type": chart_type, "height": chart_height}
+
+    return budget_shares
+    
+    
     
 @blueprint.route("/about/")
 def about():
---
+++ myCPI/static/js/graph_highcharts.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $('#'+chart_id).highcharts({
+        chart: chart,
+        title: title,
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    }
+                }
+            }
+        },      
+        series: series
+    });
+});
