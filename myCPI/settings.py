a371a0e907c99d78d703cb73dfdcfd1ee2df1e0e
Sasan Bahadaran <sasan.baha@gmail.com>
2015-10-23 13:05:11 -0400

    reverted settings.py changes

--- myCPI/settings.py
+++ myCPI/settings.py
@@ -17,11 +17,15 @@ class Config(object):
 
 class ProdConfig(Config):
     """Production configuration."""
-    ENV = 'prod'
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os_env.get('DATABASE_URL', 'postgres://test:test@localhost')
-    DEBUG_TB_ENABLED = False  # Disable Debug toolbar
-
+     ENV = 'prod'
+     DEBUG = False
+     DB_NAME = 'dev.db'
+     # Put the db file in project root
+     DB_PATH = os.path.join(Config.PROJECT_ROOT, DB_NAME)
+     SQLALCHEMY_DATABASE_URI = 'sqlite:///{0}'.format(DB_PATH)
+     # TODO: Change me
+     SQLALCHEMY_DATABASE_URI = os_env.get('DATABASE_URL', 'postgres://test:test@localhost')
+     DEBUG_TB_ENABLED = False  # Disable Debug toolbar
 
 class DevConfig(Config):
     """Development configuration."""
