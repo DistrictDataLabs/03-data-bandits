91358912f78554b73df687c3763bd9dbcf71caec
Sasan Bahadaran <sasan.baha@gmail.com>
2015-10-23 13:32:07 -0400

    removed postgres param

--- myCPI/settings.py
+++ myCPI/settings.py
@@ -22,8 +22,7 @@ class ProdConfig(Config):
     DB_NAME = 'dev.db'
     # Put the db file in project root
     DB_PATH = os.path.join(Config.PROJECT_ROOT, DB_NAME)
-    # TODO: Change me
-    SQLALCHEMY_DATABASE_URI = os_env.get('DATABASE_URL', 'postgres://test:test@localhost')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///{0}'.format(DB_PATH)
     DEBUG_TB_ENABLED = False  # Disable Debug toolbar
 
 class DevConfig(Config):
